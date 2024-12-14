0187969434bdbaf2e7ce65bcbf1e5947f1d398d2
Zaina Al Habash <75005450+zalhabash@users.noreply.github.com>
2024-12-13 22:41:13 -0500

    feat: :sparkles: solve day 1a

--- day-1-a.cpp
+++ day-1-a.cpp
@@ -1,8 +1,41 @@
+#include <algorithm>
+#include <fstream>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
-  cout << "hello world!" << endl;
+  ifstream input("inputs/day-1-a.txt");
+
+  if (!input.is_open()) {
+    cout << "Unable to open file." << endl;
+  }
+
+  int left_list_item = 0;
+  int right_list_item = 0;
+
+  vector<int> left_list;
+  vector<int> right_list;
+
+  while (input >> left_list_item >> right_list_item) {
+    left_list.push_back(left_list_item);
+    right_list.push_back(right_list_item);
+  }
+
+  if (left_list.size() != right_list.size()) {
+    cout << "Error: left and right lists are not the same size." << endl;
+  }
+
+  sort(left_list.begin(), left_list.end());
+  sort(right_list.begin(), right_list.end());
+
+  int distance = 0;
+
+  for (size_t i = 0; i < left_list.size(); i++) {
+    distance += abs(left_list[i] - right_list[i]);
+  }
+
+  cout << "distance: " << distance << endl;
   return 0;
 }
